1d18e57705c9bfe2225853e25f872fb2776c238e
sunbeichen <beichensun188@163.com>
2023-10-11 20:37:41 +0800

    set html body #root css and make a linear gradient background

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,26 @@
+export default function MainPage() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        background:
+          "linear-gradient(30deg, #656e8b 15%, #0c5e74 50%,  #17304e 100%)",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "10%",
+          left: "5%",
+
+          textAlign: "center",
+          width: "20%",
+          height: "50%",
+          background: "#fff",
+        }}
+      ></div>
+    </div>
+  );
+}
