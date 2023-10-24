e864d2ba7c8c0c9c4525f76f3041db838001976f
Tower-3620 <Oscar.YA.Zhang@outlook.com>
2023-10-24 10:59:16 +0800

    add MissionBoardPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,90 @@
+import { createContext, useState } from "react";
 import MainPage from "./pages/MainPage";
+import MissionBoardPage from "./pages/MissionBoardPage";
+import { v4 } from "uuid";
+
+export const LogList = createContext(null);
 
 function App() {
+  const [logList, setLogList] = useState([
+    {
+      type: 0,
+      title: "todo1",
+      content: "todo123",
+      pubTime: "2021-10-1",
+      planDuration: "1h",
+      ddl: "2021-10-2",
+      id: v4(),
+    },
+    {
+      type: 0,
+      content: "todo3",
+      setTime: "2021-10-1",
+      planDuration: "1h",
+      doneTime: "2021-10-2",
+      id: v4(),
+    },
+    {
+      type: 0,
+      content: "todo4",
+      setTime: "2021-10-1",
+      planDuration: "1h",
+      doneTime: "2021-10-2",
+      id: v4(),
+    },
+    {
+      type: 0,
+      content: "todo5",
+      setTime: "2021-10-1",
+      planDuration: "1h",
+      doneTime: "2021-10-2",
+      id: v4(),
+    },
+    {
+      type: 1,
+      content: "doing1",
+      id: v4(),
+    },
+    {
+      type: 2,
+      content: "done1",
+      id: v4(),
+    },
+    {
+      type: 0,
+      content: "todo2",
+      setTime: "2021-10-1",
+      planDuration: "1h",
+      doneTime: "2021-10-2",
+      id: v4(),
+    },
+    {
+      type: 1,
+      content: "doing2",
+      id: v4(),
+    },
+    {
+      type: 2,
+      content: "done2",
+      id: v4(),
+    },
+  ]);
+
   return (
-    <div
-      style={{
-        position: "relative",
-        background:
-          "linear-gradient(30deg, #656e8b 15%, #0c5e74 50%,  #17304e 100%)",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <MainPage />
-    </div>
+    <LogList.Provider value={[logList, setLogList]}>
+      <div
+        style={{
+          position: "relative",
+          background:
+            "linear-gradient(30deg, #656e8b 15%, #0c5e74 50%,  #17304e 100%)",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        {/* <MainPage /> */}
+        <MissionBoardPage />
+      </div>
+    </LogList.Provider>
   );
 }
 
---
+++ src/pages/MissionBoardPage.jsx
@@ -0,0 +1,113 @@
+import { useContext, useState } from "react";
+import { Button, Card, ListGroup } from "react-bootstrap";
+import { LogList } from "../App";
+
+export default function MissionBoardPage() {
+  const [logList, setLogList] = useContext(LogList);
+  const todos = logList.filter((log) => log.type === 0);
+
+  const MissionCard = todos.map((todo, index) => {
+    return (
+      <Card
+        key={todo.id}
+        style={{
+          width: "20em",
+          height: "25em",
+          background: "rgba(255, 255, 255, 0.5)",
+          borderRadius: "1em",
+          boxShadow: "0.5em 1em 1em rgba(0, 0, 0, 0.5)",
+          //glassmorphism effect
+          backdropFilter: "blur(5em)",
+        }}
+      >
+        <Card.Header>
+          <Card.Title
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {todo.title}
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>publish time: {todo.pubTime} </Card.Text>
+          <Card.Text>deadline: {todo.ddl} </Card.Text>
+          <Card.Text>description:</Card.Text>
+          <Card.Text
+            style={{
+              overflow: "scroll",
+              overflowX: "hidden",
+              width: "100%",
+              height: "8em",
+              border: "1px solid black",
+              padding: "0.5em",
+            }}
+          >
+            {todo.content}
+          </Card.Text>
+          <Card.Text></Card.Text>
+        </Card.Body>
+        <Card.Footer
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="primary"
+            //smaller button
+            size="sm"
+            onClick={() => {
+              const nextLogList = logList.map((log) => {
+                if (log.id === todo.id) {
+                  return {
+                    ...log,
+                    type: 1,
+                  };
+                } else {
+                  return log;
+                }
+              });
+              setLogList(nextLogList);
+            }}
+          >
+            Accept
+          </Button>
+        </Card.Footer>
+      </Card>
+    );
+  });
+
+  return (
+    //make a mission board, with a title at the top and a list of missions below
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Mission Board</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          //to the next line
+          flexWrap: "wrap",
+          gap: "2em",
+        }}
+      >
+        {MissionCard}
+      </div>
+    </div>
+  );
+}
